7d5f72a34d067c32ad51b9cd3c8f9237a7c32985
vmarialuz <vmarialuzm@gmail.com>
2024-03-26 11:44:23 -0500

    add page Vista1

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css';
+import { Vista1 } from './pages/Vista1';
 import { Vista2 } from "./pages/Vista2";
 
 
 const App = () => {
   return (
     <div>
+      <Vista1 />
       <Vista2 />
     </div>
   )
---
+++ frontend/src/pages/Vista1.jsx
@@ -0,0 +1,30 @@
+
+
+export const Vista1 = () => {
+    return (
+        <>
+            <div id="vista1">
+                <h1 id="tituloVista1">
+                    Trivia Conociendo el Perú
+                </h1>
+                
+                <img id="imagenVista1" src="https://cdn.glitch.global/489bad8f-7a52-4259-9753-3035742c418f/peruano.png?v=1711036913711" width="180" height="150" />
+                
+                <form id="formNombre">
+
+                    <label htmlFor="nombre">Ingresa tu nombre: </label><br />
+                    <input type="text" id="nombre" name="nombre" required />     
+                    
+                    <br />
+                    <div className="botonesTrivia">
+                    <button id="atractivos">Sitios turísticos</button>
+                    <button id="platos">Platos tipicos</button>
+                    </div>
+                    
+                
+                </form>
+            </div>
+        </>
+    )
+}
+
